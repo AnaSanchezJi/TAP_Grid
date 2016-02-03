bb72a6702c48a27fe07f50feae82321416f27860
OsmarPerez <osmar070595@gmail.com>
2016-02-02 22:08:06 -0700

    agregar botones

--- calculadora.java
+++ calculadora.java
@@ -1,13 +1,13 @@
 import java.awt.*;
 import javax.swing.*;
    public class calculadora extends JFrame {
-   JButton button0, button1, button2, button3, button4;
+   JButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
    JLabel label1 , label2, label3, label4, label5;
 
    public calculadora (){
-    setLayout (new GridLayout (4,5));
+    setLayout (new GridLayout (5,5));
 /*Pantalla de resultados*/
-          label1 = new JLabel("  ");
+         label1 = new JLabel("  ");
               add(label1);     
          label2 = new JLabel("  ");
               add(label2);
@@ -17,7 +17,7 @@ import javax.swing.*;
               add(label4);
           label5 = new JLabel("  ");
               add(label5);
-//botones del- 1-3 y "+" "-"
+//botones del- 1,2,3,4
 button0= new JButton("0");
 add(button0);
 
@@ -34,7 +34,26 @@ add(button3);
 
 button4= new JButton("4");  
 add(button4);
-   }
+	
+
+//Botones 4,5,6,7,8,9 
+
+	button5 = new JButton("5");
+	add(button5);
+
+	button6 = new JButton("6");
+	add(button6);
+
+	button7 = new JButton("7");
+	add(button7);
+
+	button8 = new JButton("8");
+	add(button8);
+
+	button9 = new JButton("9");
+	add(button9); 
+
+  }
  public static void main(String args[]){
 calculadora abc = new calculadora();
 abc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
