a2cb0df61de82b930efae3eadc2348f10cb5928d
AnaSanchezJi <anpasanchezji@ittepic.edu.mx>
2016-02-02 21:12:06 -0600

    Correcion

--- calculadora.java
+++ calculadora.java
@@ -2,21 +2,21 @@ import java.awt.*;
 import javax.swing.*;
    public class calculadora extends JFrame {
    JButton button1, button2, button3;
-   JLabel label1 , label2, label3;
+   JLabel label1 , label2, label3, label4, label5;
 
    public calculadora (){
     setLayout (new GridLayout (5,4));
 /*Pantalla de resultados*/
-     label1=new JLabel(" ");
-     add(label1);
-    label2=new JLabel(" ");
-     add(label1);
-     label3=new JLabel(" ");
-     add(label3);
-     label4=new JLabel(" ");
-     add(labe4);
-    label5=new JLabel(" ");
-     add(label5);
+          label1 = new JLabel("  ");
+              add(label1);     
+         label2 = new JLabel("  ");
+              add(label2);
+         label3 = new JLabel("  ");
+              add(label3);
+          label4 = new JLabel("  ");
+              add(label4);
+          label5 = new JLabel("  ");
+              add(label5);
 //-----------------------
   
     }
